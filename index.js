fa12573f9d1722f4297746de629e1a6bee4f9100
VQuine <01010110.v@gmail.com>
2021-06-24 16:25:21 -0400

    commit tree 44e98ce798cd99b70e7aa293342f509c474a0bae

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  
+  // capitalize first letter
+  static capitalize(string) {
+    let a = string.split('')
+    a[0] = a[0].toUpperCase()
+    return a.join('')
+  }
+  // https://www.digitalocean.com/community/tutorials/js-capitalizing-strings
+  // uses regex
+
 }
